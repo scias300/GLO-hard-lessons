7a90c75e3c819a523967aae04ee067d86e195f76
Alexander Pashkov <scias300@gmail.com>
2022-05-30 22:00:07 +0300

    Выполнил усложненное задание третьего урока

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ console.log(accum);
 
 let accumPow = accum ** 3;
 console.log(accumPow.toString().slice(0, 2));
+
+let lang = prompt('Введите язык в формате ru или en');
+const weekRu = 'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье';
+const weekEn = 'Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday';
+let arr = [];
+arr.ru = weekRu;
+arr.en = weekEn;
+
+while (lang !== 'ru' && lang !== 'en') {
+    lang = prompt('Введите язык в формате ru или en');
+}
+
+if (lang === 'ru') {
+    console.log(weekRu);
+} else if (lang === 'en') {
+    console.log(weekEn);
+}
+
+switch (lang) {
+    case 'ru':
+        console.log(weekRu);
+        break;
+    case 'en':
+        console.log(weekEn);
+        break;
+}
+
+console.log(arr[lang]);
+
+const namePerson = prompt('Кто ты?');
+
+let role = (namePerson === 'Артем') ? console.log('директор') :
+    (namePerson === 'Александр') ? console.log('преподаватель') :
+        console.log('студент');
