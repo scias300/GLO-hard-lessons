200195e693315da608a07bc249a6620766446382
Alexander Pashkov <scias300@gmail.com>
2022-06-04 17:20:56 +0300

    усложненное задание 2 урока 5

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const num = 266219;
-const numStr = num.toString();
-let accum = 1;
+let arr = [
+    '123',
+    '456',
+    '789',
+    '987654321',
+    '951753',
+    '258963741',
+    '333333333333333333'
+];
 
-for (let i = 0; i < numStr.length; i++) {
-    accum *= numStr[i];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].charAt(0) == 2 || arr[i].charAt(0) == 4) {
+        console.log(arr[i]);
+    }
 }
-console.log(accum);
 
-let accumPow = accum ** 3;
-console.log(accumPow.toString().slice(0, 2));
+for (let i = 1; i <= 100; i++) {
+    let counter = 0;
+    for (let j = 1; j <= i; j++) {
+        if (i % j === 0) {
+            counter++;
+        }
+    }
+    if (counter === 2) {
+        console.log(i, 'Делители этого числа: 1 и ' + i);
+    }
+}
